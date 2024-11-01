deb429e9003fcc15362379fb6338db6b77ca0acb
유윤지 <ll.o.llwhkr@gmail.com>
2024-11-01 13:49:03 +0900

    fix: location.hostname설정

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ const initProduct = {
   price:0,
 }
 
-axios.defaults.baseURL = import.meta.PROD || "http://localhost:8080"
+axios.defaults.baseURL = import.meta.PROD ? location.hostname: "http://localhost:8080";
 
 const getAllProductsApi = async () =>{
   const { data } = await axios({
